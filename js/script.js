6f24614ba70980aaba3378f9eda5d29e77da46a0
yuliia-web-dev <yuliia.devweb@gmail.com>
2025-03-21 10:40:50 +0200

    popup

--- js/script.js
+++ js/script.js
@@ -80,12 +80,7 @@ function popupClose(popupActive, doUnlock = true) {
 function bodyLock() {
 	const lockPaddingValue = window.innerWidth - document.querySelector('.wrapper').offsetWidth + 'px';
 
-	// if (lockPadding.length > 0) {
-	// 	for (let index = 0; index < lockPadding.length; index++) {
-	// 		const el = lockPadding[index];
-	// 		el.style.paddingRight = lockPaddingValue;
-	// 	}
-	// }
+
 	body.style.paddingRight = lockPaddingValue;
 	body.classList.add('lock');
 
